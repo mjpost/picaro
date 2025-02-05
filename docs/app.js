25df34de4003ab6696ea0bd4c143e9c494146100
Matt Post <mattpost@microsoft.com>
2025-02-05 10:00:13 -0800

    Update

--- docs/app.js
+++
@@ -1,6 +0,0 @@
-import { createElement } from 'react'
-import { createRoot } from 'react-dom/client'
-import { WordAligner } from './word-aligner.js'
-
-const root = createRoot(document.getElementById('root'))
-root.render(createElement(WordAligner))
